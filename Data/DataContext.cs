e0bf987b336a21e7ed9d6e1aa301647254a2f8bc
marc <777marc@gmail.com>
2018-08-05 07:08:48 -0400

    intial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+using MarcBlog.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarcBlog.API.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly DbContextOptions<DataContext> _options;
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ Models/Value.cs
@@ -0,0 +1,8 @@
+namespace MarcBlog.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
