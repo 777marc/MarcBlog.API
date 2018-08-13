9875dff8f13e60142165f3b9504249d0585fc458
marc <777marc@gmail.com>
2018-08-13 18:10:30 -0400

    auth

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -7,7 +7,7 @@ namespace MarcBlog.API.Data
     {
         private readonly DbContextOptions<DataContext> _options;
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
-        public DbSet<Value> Values { get; set; }
         public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
