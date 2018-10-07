efd342584d63315afa2745e17673365cf6b6cb2a
Marc Mendoza <777marc@gmail.com>
2018-10-07 06:43:36 -0400

    production build

--- Startup.cs
+++ Startup.cs
@@ -57,7 +57,14 @@ namespace MarcBlog.API
 
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseAuthentication();
-            app.UseMvc();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapSpaFallbackRoute(
+                    name: "spa-fallback",
+                    defaults: new { controller = "Fallback", action = "Index" }
+                );
+            });
         }
     }
 }
